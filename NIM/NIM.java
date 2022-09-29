327aa2c48e5dfd8f6a4233e196e43a072c8bb43f
JiundJi <zjiyang0@gmail.com>
2022-09-29 16:44:31 +0200

    added content

--- NIM/NIM.java
+++ NIM/NIM.java
@@ -1,3 +1,4 @@
+package NIM;
 public class NIM {
   public static void main (String[]args) {
     
--- NIM/Streichholzhaufen.java
+++ NIM/Streichholzhaufen.java
@@ -1,3 +1,4 @@
+package NIM;
 public class Streichholzhaufen {
   private int streichholzAnzahl;
   public Streichholzhaufen (int anzahl) {
@@ -6,5 +7,24 @@ public class Streichholzhaufen {
   public Streichholzhaufen() {
     this.streichholzAnzahl = 20;
   }
+  public int getStreichholzAnzahl() return streichholzAnzahl;
   
+  public boolean removeStreichholz(int anzahl) {
+    if (anzahl == null) return false;
+    if (anzahl > streichholzAnzahl) {
+      streichholzAnzahl -= anzahl;
+      return true;
+    }
+      return false;
+  }
+  
+  public boolean isEmpty() {
+    if (streichholzAnzahl <= 0) return true;
+    return false;
+  }
+
+  public boolean isValidAnzahl(int anzahl) {
+    if (anzahl != null && anzahl >= 1 && anzahl <= 3) return true;
+    return false;
+  }
 }
