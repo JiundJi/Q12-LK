320d70464db55c37cce6b82d5bff2195c9333e78
JiundJi <zjiyang0@gmail.com>
2022-11-01 11:17:45 +0100

    yes

--- BanderSnatch but not really/nvm/MathTeacher.java
+++ BanderSnatch but not really/nvm/MathTeacher.java
@@ -1,5 +1,19 @@
 package nvm;
 
 public class MathTeacher extends Teacher{
-    
+    public MathTeacher(Room room, String name) {
+        super(room, name);
+    }
+
+    @Override
+    public void discuss() {
+        util.println("yes?");
+        if((int) (Math.random() * 101) < 75) {
+            xp += 5;
+            util.println("uwu");
+        } else {
+            xp--;
+            util.println("qwq");
+        }
+    }
 }
